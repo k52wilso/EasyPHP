df3233f0bd74e6494cedfd84a763fc7f4c5aaf19
kylwil29 <kylwil29@ps553095.dreamhostps.com>
2017-01-03 19:26:11 -0800

    added dbconnect class

---
+++ dbconnect.php
@@ -0,0 +1,35 @@
+<?php
+
+	class DBconnect {
+		private $connection ; // to connect to MySQL
+		private $tableToConnect;
+		private $dbName ;
+		private $dbPassword ;
+		private $dbUser ;
+
+		/*Connect to db passing */
+		function __construct ($dbName){
+			$this->dbName = $dbName;
+		}
+
+		//Function: connect to database
+		function connect(){
+
+		}
+
+
+		//Function : Send data to database
+		//Parameter : -table to access
+		//			  - id (blank string if not needed)
+		function get(table,id) {
+
+		}
+
+		//Function : Send data to database
+		//Parameter : An array containing information (such as an id or multiple ids) to update
+		function post(array) {
+
+		}
+	}
+
+?>
